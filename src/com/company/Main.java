a4a00b0026116d94c7d12b1855f870b1b84db331
serian <sseriann@mail.ru>
2020-02-10 15:07:09 +0400

    4_5

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,30 +2,17 @@ package com.company;
 
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) {
-        Person person1 = new Person(20);
-        Person person2 = new Person(30);
-        Person person3 = new Person(50);
-        ArrayList<Person> arrayList = new ArrayList<>();
-        arrayList.add(person1);
-        arrayList.add(person2);
-        arrayList.add(person3);
-        Integer maxAge = arrayList.stream()
-                .map(Person::getAge)
-                .max(Integer::compare).get();
-
-
-        List<Person> collect = arrayList.stream()
-                .filter( (each)-> each.getAge() == maxAge)
-                .collect(Collectors.toList());
-        collect.forEach((each)-> System.out.println(each));
-
-
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add( 4 );
+        numbers.add( 5 );
+        numbers.add( 8 );
+        numbers.add( 10 );
+        numbers.add( 9 );
+        int sum = numbers.stream().mapToInt(Integer::intValue).sum();
+        System.out.println(sum);
     }
 }
