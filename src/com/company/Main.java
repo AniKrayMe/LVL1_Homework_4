7c5bda2b50716bcf61a64c0554de2994023ee0d2
serian <sseriann@mail.ru>
2020-02-10 14:56:57 +0400

    4_4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,31 @@
 package com.company;
 
-import java.util.Arrays;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) {
-        List<String> list = Arrays.asList("5.6", "7.4", "4",
-                "1", "2.3");
-        list.stream().flatMap(num -> Stream.of(num)).
-                forEach(System.out::println);
+        Person person1 = new Person(20);
+        Person person2 = new Person(30);
+        Person person3 = new Person(50);
+        ArrayList<Person> arrayList = new ArrayList<>();
+        arrayList.add(person1);
+        arrayList.add(person2);
+        arrayList.add(person3);
+        Integer maxAge = arrayList.stream()
+                .map(Person::getAge)
+                .max(Integer::compare).get();
+
+
+        List<Person> collect = arrayList.stream()
+                .filter( (each)-> each.getAge() == maxAge)
+                .collect(Collectors.toList());
+        collect.forEach((each)-> System.out.println(each));
+
 
     }
 }
---
+++ src/com/company/Person.java
@@ -0,0 +1,42 @@
+package com.company;
+
+import java.util.Objects;
+
+public class Person {
+    private int age;
+
+    public Person(int age) {
+        this.age = age;
+    }
+
+    public Person() {
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "age=" + age +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(age);
+    }
+}
